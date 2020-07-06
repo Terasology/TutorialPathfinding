a52737f460de9fb5fa6f07f7414b1493cd4ded78
agent-q1 <suhassuhas2001@gmail.com>
2020-07-06 13:16:58 +0530

    feat: Added BaseFlatWorldRasterizer

---
+++ src/main/java/org/terasology/tutorialpathfinding/world/BaseFlatWorldRasterizer.java
@@ -0,0 +1,37 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.tutorialpathfinding.world;
+
+import org.terasology.math.ChunkMath;
+import org.terasology.math.JomlUtil;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.CoreChunk;
+import org.terasology.world.generation.Region;
+import org.terasology.world.generation.WorldRasterizer;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
+
+public class BaseFlatWorldRasterizer implements WorldRasterizer {
+    private Block dirt;
+
+    @Override
+    public void initialize() {
+        dirt = CoreRegistry.get(BlockManager.class).getBlock("CoreAssets:Dirt");
+    }
+
+    @Override
+    public void generateChunk(CoreChunk chunk, Region chunkRegion) {
+        SurfaceHeightFacet surfaceHeightFacet = chunkRegion.getFacet(SurfaceHeightFacet.class);
+        for (Vector3i position : chunkRegion.getRegion()) {
+            float surfaceHeight = surfaceHeightFacet.getWorld(position.x, position.z);
+            if (position.y < surfaceHeight) {
+                Vector3i chunkPos = ChunkMath.calcRelativeBlockPos(position);
+                org.joml.Vector3i pos =  JomlUtil.from(chunkPos);
+                chunk.setBlock(pos, dirt);
+            }
+        }
+    }
+}
