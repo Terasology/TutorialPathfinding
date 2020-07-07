269072b6b9d9ae3ebcd80262aeab24640bbb323b
agent-q1 <suhassuhas2001@gmail.com>
2020-07-07 14:58:41 +0530

    feat: Added prefab for base characters

---
+++ src/main/java/org/terasology/tutorialpathfinding/events/CharacterSpawnEvent.java
@@ -0,0 +1,4 @@
+package org.terasology.tutorialpathfinding.events;
+
+public class CharacterSpawnEvent {
+}
---
+++ src/main/java/org/terasology/tutorialpathfinding/systems/SpawnSystem.java
@@ -0,0 +1,4 @@
+package org.terasology.tutorialpathfinding.systems;
+
+public class SpawnSystem {
+}
