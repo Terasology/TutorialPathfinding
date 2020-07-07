6aa35421069d5f53c63ded88cf5a26ca9e661f9a
agent-q1 <suhassuhas2001@gmail.com>
2020-07-07 15:00:02 +0530

    Added a citizen spawn event

--- src/main/java/org/terasology/tutorialpathfinding/events/CharacterSpawnEvent.java
+++ src/main/java/org/terasology/tutorialpathfinding/events/CharacterSpawnEvent.java
@@ -1,4 +1,30 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
 package org.terasology.tutorialpathfinding.events;
 
-public class CharacterSpawnEvent {
+import org.joml.Vector3f;
+import org.terasology.entitySystem.event.Event;
+import org.terasology.entitySystem.prefab.Prefab;
+
+
+public class CharacterSpawnEvent implements Event {
+    private Prefab prefab;
+    private Vector3f location;
+
+    public CharacterSpawnEvent() {
+    }
+
+    public CharacterSpawnEvent(Prefab prefab, Vector3f location) {
+        this.prefab = prefab;
+        this.location = location;
+    }
+
+    public Prefab getCharacterPrefab() {
+        return this.prefab;
+    }
+
+    public Vector3f getSpawnPosition() {
+        return this.location;
+    }
 }
